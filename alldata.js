e1c05d45ec2c88c4610766945dbbe1f671e71733
Alok Kumar Maurya <akmaurya1206@gmail.com>
2025-06-26 07:40:17 +0530

    button change

--- alldata.js
+++ alldata.js
@@ -38,12 +38,12 @@ const cardItemName = [
         "old_price": "₹100",
         "new_price": "₹100",
         "rating": "4.8/5",
-        "user" : "hfflmwk",
+        "user": "hfflmwk",
         "like": "ri-heart-3-line",
         "description": "Chicken Curry is a popular Indian dish made.",
         "btn": "ADD TO CART",
         "buy_count": "12452",
-        "like_count" : "4/58"
+        "like_count": "4/58"
     },
     {
         "id": 2,
@@ -52,12 +52,12 @@ const cardItemName = [
         "old_price": "₹100",
         "new_price": "₹100",
         "rating": "4.8/5",
-        "user" : "hfflmwk",
+        "user": "hfflmwk",
         "like": "ri-heart-3-line",
         "description": "Chicken Curry is a popular Indian dish made.",
         "btn": "ADD TO CART",
         "buy_count": "12452",
-        "like_count" : "4/58"
+        "like_count": "4/58"
     },
     {
         "id": 3,
@@ -66,12 +66,12 @@ const cardItemName = [
         "old_price": "₹1000",
         "new_price": "₹399",
         "rating": "4.8/5",
-        "user" : "hfflmwk",
+        "user": "hfflmwk",
         "like": "ri-heart-3-line",
         "description": "Chicken Curry is a popular Indian dish made.",
         "btn": "ADD TO CART",
         "buy_count": "12452",
-        "like_count" : "4/58"
+        "like_count": "4/58"
     },
     {
         "id": 4,
@@ -80,12 +80,12 @@ const cardItemName = [
         "old_price": "₹1990",
         "new_price": "₹1199",
         "rating": "4.8/5",
-        "user" : "hfflmwk",
+        "user": "hfflmwk",
         "like": "ri-heart-3-line",
         "description": "Chicken Curry is a popular Indian dish made.",
         "btn": "Buy Now",
         "buy_count": "12452",
-        "like_count" : "4/58"
+        "like_count": "4/58"
     },
     {
         "id": 5,
@@ -94,63 +94,124 @@ const cardItemName = [
         "old_price": "₹100",
         "new_price": "₹100",
         "rating": "4.8/5",
-        "user" : "hfflmwk",
+        "user": "hfflmwk",
         "like": "ri-heart-3-line",
         "description": "Chicken Curry is a popular Indian dish made.",
         "btn": "ADD TO CART",
         "buy_count": "12452",
-        "like_count" : "4/58"
+        "like_count": "4/58"
     }
 
 
 
 
 
-            ]
+]
 
 const productDitals2 = [
-{
-    "name": "Chicken Curry",
-    "image": "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "price-old": "₹100",
-    "price-new": "₹100",
-    "new-user": "NEW USER OFFER",
-    "rating": "4.8/5",
-    "like-count": "4",
-    "description": "Chicken Curry is a popular Indian dish made.",
-    "serves": "10 Min",
-    "rating": "Bristol",
-   
-},
+    {
+        "name": "Chicken Curry",
+        "image": "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "price-old": "₹100",
+        "price-new": "₹100",
+        "new-user": "NEW USER OFFER",
+        "rating": "4.8/5",
+        "like-count": "4",
+        "description": "Chicken Curry is a popular Indian dish made.",
+        "serves": "10 Min",
+        "rating": "Bristol",
 
-{
-    "name": "Chicken Curry",
-    "image": "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "price": "₹100",
-    "rating": "4.8/5",
-    "like": "4",
-    "description": "Chicken Curry is a popular Indian dish made.",
-    "serves": "10 Min",
-    "rating": "Bristol",
-    "socialMedia": [
-        {
-            "name": "Facebook",
-            "link": "https://www.facebook.com/"
-        },
-        {
-            "name": "Instagram",
-            "link": "https://www.instagram.com/"
-        },
-        {
-            "name": "Whatsapp",
-            "link": "https://www.whatsapp.com/"
-        },
-        {
-            "name": "Youtube",
-            "link": "https://www.youtube.com/"
-        }
-    ]
-}
+    },
+
+    {
+        "name": "Chicken Curry",
+        "image": "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "price": "₹100",
+        "rating": "4.8/5",
+        "like": "4",
+        "description": "Chicken Curry is a popular Indian dish made.",
+        "serves": "10 Min",
+        "rating": "Bristol",
+        "socialMedia": [
+            {
+                "name": "Facebook",
+                "link": "https://www.facebook.com/"
+            },
+            {
+                "name": "Instagram",
+                "link": "https://www.instagram.com/"
+            },
+            {
+                "name": "Whatsapp",
+                "link": "https://www.whatsapp.com/"
+            },
+            {
+                "name": "Youtube",
+                "link": "https://www.youtube.com/"
+            }
+        ]
+    }
 
 
 ]
+
+
+
+let buttonList = document.getElementById("button-list");
+const btnListName2 = [
+    {
+        "id": 1,
+        "name": "Coffee",
+        "image": "https://www.citypng.com/public/uploads/preview/brown-coffee-cup-logo-design-hd-png-701751694776791z8t9jck4mz.png",
+        "link": "./coffee/index.html"
+    },
+    {
+        "id": 2,
+        "name": "Pizza",    
+        "image": "https://static.vecteezy.com/system/resources/thumbnails/011/157/909/small_2x/pizzeria-emblem-on-blackboard-pizza-logo-template-emblem-for-cafe-restaurant-or-food-delivery-service-vector.jpg"
+    },
+    {
+        "id": 3,
+        "name": "Paneer Tikka",
+        "image": "https://media.istockphoto.com/id/1186759790/photo/paneer-tikka-at-skewers-in-black-bowl-at-dark-slate-background-paneer-tikka-is-an-indian.jpg?s=612x612&w=0&k=20&c=cITToqM1KEnrixXjoLhEciqP24SxdKtW3QXykq-W5OE="
+    },
+    {
+        "id": 4,
+        "name": "Chicken",
+        "image": "https://img.freepik.com/premium-vector/chicken-logo-design-vector-template-creative-logo-brand_628348-64.jpg"
+    } , {
+        "id": 5,
+        "name": "Chili Sauce",
+        "image": "https://static.vecteezy.com/system/resources/thumbnails/048/201/639/small/spicy-chili-sauce-logo-design-the-concept-of-chilli-and-fire-for-sauce-products-spicy-foods-and-others-vector.jpg"
+    },
+    {
+        "id": 6,
+        "name": "Noodles",
+        "image": "https://t4.ftcdn.net/jpg/01/39/05/09/360_F_139050937_LkiKRCb97x2Ybc5urilz314mLQRUcHfn.jpg"
+    },
+    {
+        "id": 7,
+        "name": "Burger",
+        "image": "https://www.logoground.com/uploads11/dv11y20231586352023-02-034285667jeremie_Bursting-Cheese-Burger.jpg"
+    }   
+
+]
+function btnList() {
+    let btnHTML = "";
+    btnListName2.forEach((items, key) => {
+        btnHTML += `
+        
+
+         <li>
+             <a href="${items.link}">
+                <div class="li-img">
+                                <img src="${items.image}" alt="">
+                </div>
+                <span>${items.name}</span>
+             </a>
+        </li>
+        `
+    })
+    buttonList.innerHTML = btnHTML
+}
+btnList()
